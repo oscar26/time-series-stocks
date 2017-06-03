972f9799b81cda9625422200a851305a3f541e48
oscar26 <deangel95@gmail.com>
2017-06-03 15:06:37 -0500

    Base naive predictor completed

--- naivepredictor.py
+++ naivepredictor.py
@@ -12,42 +12,57 @@ from keras.layers import Input, Dense
 class NaivePredictor(object):
 	"""docstring for NaivePredictor"""
 	def __init__(self, dataset_path,	input_window_size=4, rolling_window_size=5,
-			columns_to_standardize=None, columns_to_windowize=None):
+			columns_to_standardize=None, columns_to_windowize=None, data=None):
 		self.dataset_path = dataset_path
 		self.input_window_size = input_window_size
 		self.rolling_window_size = rolling_window_size
 		self.columns_to_standardize = columns_to_standardize
 		self.columns_to_windowize = columns_to_windowize
+		self.data = data
 		self.__preprocess_data()
 		self.__create_model()
 
-	def test_model(self, epochs=100, verbose=2):
+	def test_model(self, n_splits=9, cv_runs=10, epochs=100, verbose=2):
 		"""Evaluación del modelo usando validación cruzada
 		hacia adelante."""
 		from sklearn.model_selection import TimeSeriesSplit
 
+		self.metrics = ['MSE', 'RMSE', 'MAE', 'MAPE']
+		train_scores = np.zeros((cv_runs, n_splits, len(self.metrics)))
+		test_scores = np.zeros((cv_runs, n_splits, len(self.metrics)))
 		fmt = DataFormatter()
-		tscv = TimeSeriesSplit(n_splits=9)
-		i = 1
-		for train_index, test_index in tscv.split(self.data['Close'].values):
-			# División del conjunto de datos en entrenamiento y prueba
-			train_df = self.data.iloc[train_index]
-			test_df = self.data.iloc[test_index]
-			# Estandarización del conjunto de datos
-			train_data, training_means, training_stds = self.__standardize_features(train_df, self.columns_to_standardize)
-			test_data = self.__standardize_features_for_test(test_df, self.columns_to_standardize, training_means, training_stds)
-			# Extracción de ventanas de datos
-			trainX, trainY = fmt.windowize_series(train_data.as_matrix(), size=self.input_window_size, column_indexes=self.columns_to_windowize)
-			testX, testY = fmt.windowize_series(test_data.as_matrix(), size=self.input_window_size, column_indexes=self.columns_to_windowize)
-			# Ajustando el modelo
-			print('\nFold %i' % (i))
-			self.model.fit(trainX, trainY, epochs=epochs, batch_size=32, validation_data=(testX, testY), verbose=verbose)
-			# Evaluando cada partición de la validación cruzada hacia adelante
-			trainScore = self.model.evaluate(trainX, trainY, verbose=verbose)
-			print('Train Score: %.5f MSE, %.5f RMSE, %.5f MAE, %.5f%% MAPE' % (trainScore[0], np.sqrt(trainScore[0]), trainScore[1], trainScore[2]*100))
-			testScore = self.model.evaluate(testX, testY, verbose=verbose)
-			print('Test Score: %.5f MSE, %.5f RMSE, %.5f MAE, %.5f%% MAPE\n' % (testScore[0], np.sqrt(testScore[0]), testScore[1], testScore[2]*100))
-			i += 1
+		tscv = TimeSeriesSplit(n_splits=n_splits)
+		for j in xrange(cv_runs):
+			# print('\nCross-validation run %i' % (j+1))
+			i = 1
+			for train_index, test_index in tscv.split(self.data['Close'].values):
+				# División del conjunto de datos en entrenamiento y prueba
+				train_df = self.data.loc[train_index]
+				test_df = self.data.loc[test_index]
+				# Estandarización del conjunto de datos
+				train_data, training_means, training_stds = self.__standardize_features(train_df, self.columns_to_standardize)
+				test_data = self.__standardize_features_for_test(test_df, self.columns_to_standardize, training_means, training_stds)
+				# Extracción de ventanas de datos
+				trainX, trainY = fmt.windowize_series(train_data.as_matrix(), size=self.input_window_size, column_indexes=self.columns_to_windowize)
+				testX, testY = fmt.windowize_series(test_data.as_matrix(), size=self.input_window_size, column_indexes=self.columns_to_windowize)
+				# Ajustando el modelo
+				# print('Fold %i' % (i))
+				self.model.fit(trainX, trainY, epochs=epochs, batch_size=32, validation_data=(testX, testY), verbose=verbose)
+				# Evaluando cada partición de la validación cruzada hacia adelante
+				train_score = self.model.evaluate(trainX, trainY, verbose=verbose)
+				train_score = np.array([train_score[0], np.sqrt(train_score[0]), train_score[1], train_score[2]*100])
+				test_score = self.model.evaluate(testX, testY, verbose=verbose)
+				test_score = np.array([test_score[0], np.sqrt(test_score[0]), test_score[1], test_score[2]*100])
+				# print('Train Score: %.5f MSE, %.5f RMSE, %.5f MAE, %.5f%% MAPE' % (train_score[0], train_score[1], train_score[2], train_score[3]))
+				# print('Test Score: %.5f MSE, %.5f RMSE, %.5f MAE, %.5f%% MAPE\n' % (test_score[0], test_score[1], test_score[2], test_score[3]))
+				# [0: MSE, 1: RMSE, 2: MAE, 3: MAPE]
+				train_scores[j, i-1, :] = train_score
+				test_scores[j, i-1, :] = test_score
+				i += 1
+		self.train_results = train_scores.mean(axis=0).mean(axis=0)
+		self.test_results = test_scores.mean(axis=0).mean(axis=0)
+		# print(train_results)
+		# print(test_results)
 
 	def compile_model(self):
 		metrics = ['mean_squared_error', 'mean_absolute_error', 'mean_absolute_percentage_error']
@@ -97,15 +112,18 @@ class NaivePredictor(object):
 
 	def __preprocess_data(self):
 		"""Preprocesamiento del conjunto de datos."""
-		df = pd.read_csv(self.dataset_path)
-		df['Close'] = df['Adj Close'] # DELETE
-		date = pd.to_datetime(df['Date'])
-		df.insert(0, 'Month', date.dt.month)
-		df.insert(1, 'Day', date.dt.day)
-		df = df.drop('Adj Close', axis=1)
-		df = df.drop('Date', axis=1)
-		new_column_order = ['Month', 'Day', 'Volume', 'Open', 'High', 'Low', 'Close']
-		self.data = df.reindex(columns=new_column_order)
+		if (self.data is None):
+			df = pd.read_csv(self.dataset_path)
+			df['Close'] = df['Adj Close'] # DELETE
+			date = pd.to_datetime(df['Date'])
+			df.insert(0, 'Month', date.dt.month)
+			df.insert(1, 'Day', date.dt.day)
+			df = df.drop('Adj Close', axis=1)
+			df = df.drop('Date', axis=1)
+			new_column_order = ['Month', 'Day', 'Volume', 'Open', 'High', 'Low', 'Close']
+			self.data = df.reindex(columns=new_column_order)
+		else:
+			pass
 
 def test():
 	"""Método exclusivo para pruebas locales de funcionamiento."""
@@ -118,7 +136,7 @@ def test():
 		columns_to_windowize=columns_to_windowize,
 		input_window_size=input_window_size)
 	predictor.compile_model()
-	predictor.test_model(epochs=90, verbose=0)
+	predictor.test_model(n_splits=9, epochs=90, verbose=0)
 
 if __name__ == '__main__':
 	test()
